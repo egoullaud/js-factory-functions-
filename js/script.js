91a208b86ba2e2aefff6498c8cda69b1abde2561
elizabeth.goullaud <egoullaud@gmail.com>
2022-08-10 12:27:51 -0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const button = document.querySelector("button");
+const repairList = document.querySelector(".need-repair");
+
+const createVehicle = function (type, numWheels, color) {
+  const vehicle = {};
+  vehicle.type = type;
+  vehicle.numWheels = numWheels;
+  vehicle.color = color;
+
+  return vehicle;
+};
+
+const car = createVehicle("Car", 4, "blue");
+const bike = createVehicle("bike", 2, "white");
+const skateboard = createVehicle("skateboard", 4, "black");
+
+const myVehicles = [car, bike, skateboard];
+
+for (let vehicle of myVehicles) {
+  for (let key in vehicle) {
+    console.log(key, vehicle[key]);
+  }
+}
+
+bike.needsRepair = true;
+skateboard.needsRepair = true;
+
+button.addEventListener("click", function () {
+  repairList.innerHTML = "";
+  const vehicleRepairList = myVehicles.filter(function (vehicle) {
+    return vehicle.needsRepair === true;
+  });
+
+  for (let vehicle of vehicleRepairList) {
+    let li = document.createElement("li");
+    li.innerHTML = `My <span> ${vehicle.type}</span> needs some attention.`;
+    repairList.append(li);
+  }
+});
